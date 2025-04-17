21b92fe7ac7f5e2fa62b46f2dfaa04ba61fddcf7
steveharsant <stevenharsant@gmail.com>
2025-04-17 14:01:18 +1000

    undo

--- scripts/sshfs_mount.sh
+++ scripts/sshfs_mount.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-[ -z "$BACKUP_NAME" ] && exit 1
-[ -z "$BACKUP_PATH" ] && exit 1
-[ -z "$REMOTE_USER" ] && exit 1
-[ -z "$REMOTE_HOST" ] && exit 1
-[ ! -s "$SSHKEY_PATH" ] && exit 1
-connection_string="$REMOTE_USER@$REMOTE_HOST:$BACKUP_PATH"
+set -e
+
+[ -z "$1" ] && exit 1 || backup_name="$1"
+[ -z "$2" ] && exit 1 || connection_string="$2"
+[ -z "$3" ] && PRIVATE_KEY_PATH="${PRIVATE_KEY_PATH:-}" || PRIVATE_KEY_PATH="$3"
+[ -z "$PRIVATE_KEY_PATH" ] && exit 1
+
+echo 'Parsed script arguments successfully'
+
+if ! [ -s "$PRIVATE_KEY_PATH" ]; then
+  echo "Private key not found" && exit 1
+fi
 
 apk add sshfs
-mkdir -p "/$BACKUP_NAME"
-sshfs -o IdentityFile="$SSHKEY_PATH",StrictHostKeyChecking=no \
-      "$connection_string" "/$BACKUP_NAME"
+echo 'sshfs dependency met'
 
-exit 0
+mkdir -p "/mnt/$backup_name"
+sshfs -o IdentityFile="$PRIVATE_KEY_PATH",StrictHostKeyChecking=no \
+      "$connection_string" "/mnt/$backup_name"
+
+echo "sshfs path successfully mounted from '$connection_string' to '/mnt/$backup_name'"
